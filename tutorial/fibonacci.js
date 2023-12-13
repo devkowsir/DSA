8b5280d89d5249446a83345c7f6285ce7ddf8bf7
kawsarahmed-012 <mdkowsir369@gmail.com>
2023-12-13 17:32:38 +0600

    algorithm of finding fibonacci sequence

---
+++ tutorial/fibonacci.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @returns {number[]}
+ */
+function fibonacci(n) {
+  const fib = [0, 1];
+
+  for (i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+}
+
+console.log(fibonacci(10));
